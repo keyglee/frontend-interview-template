2aea8b3e90afefd8a64feb5913b2ef58ce8b90ba
Wyatt Turner <wyatt.turner.002@gmail.com>
2024-12-27 10:36:16 -0500

    Updating documentation + template

--- todo-demo/app/page.tsx
+++ todo-demo/app/page.tsx
@@ -1,101 +1,66 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="h-screen w-screen flex-col space-y-6 p-6">
+      <div className="place-self-center space-y-3 w-1/2">
+        <h1 className="text-4xl font-bold">
+          Welcome to the Keyglee Frontend Interview Template!
+        </h1>
+        <p>
+          Feel free to alter or delete any of the code present in this
+          next.js/react app.
+        </p>
+        <p>
+          Alternatively you may use whatever frontend tools you feel more
+          comfortable with!
+        </p>
+      </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <div className="place-self-center space-y-3 w-1/2">
+        <h2 className="text-2xl font-bold">Assignment Details</h2>
+        <p>
+          Create a basic TODO app using the provided backend. You shouldn&apos;t
+          need to alter the backend, just worry about making the proper CRUD api
+          calls to it.
+        </p>
+        <h4>Goals</h4>
+        <ul>
+          <li>Functionality to read the current TODOs from the API</li>
+          <li>Functionality to create new TODOs from the frontend to the API</li>
+          <li>Show off your coding style!</li>
+        </ul>
+        <h4>Optional</h4>
+        <ul>
+          <li>Functionality to update existing TODOs</li>
+          <li>Functionality to delete existing TODOs</li>
+        </ul>
+        <p>Try to limit yourself to no more than 2 hours. We really don&apos;t want you spending all your free time working on this.</p>
+        <h3 className="text-xl font-bold">The Frontend</h3>
+        <p>
+          The frontend provided is a starter for a react/next.js app. Feel free
+          to build off of this or totally remove everything in `todo-demo` to
+          make your own frontend app.
+        </p>
+
+        <h4>Tools being used Starter</h4>
+        <ul>
+          <li>Next.js</li>
+          <li>React.js</li>
+          <li>Tailwindcss</li>
+          <li>Typescript</li>
+        </ul>
+        <p>Tailwindcss is currently disabled via the globals.css file, feel free to re-enable it.</p>
+        <h3 className="text-xl font-bold">The Backend</h3>
+        <p>
+          The backend is a simple REST API that you can interact with via API
+          calls from your frontend. By default the backend&apos;s base url will
+          be `localhost:4000` with additional routes for the resource type
+          itself (for example: GET `localhost:4000/todo` will get all todos).
+        </p>
+        <p>
+          Additional API routes and documentation is provided in the
+          repository&apos;s README.
+        </p>
+      </div>
     </div>
   );
 }
